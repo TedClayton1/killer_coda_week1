12739fb75b98febdc0b1783b167c4c7be72e7cc9
Ted <ted_clayton@hotmail.com>
2026-06-24 19:22:14 -0400

    week 1 basic completed

---
+++ week_1_kila_coda.py
@@ -0,0 +1,18 @@
+my_age = 49
+current_year = 2026
+birth_year = current_year - my_age
+print(birth_year)
+
+username = input("What is your name? ")
+emotion = input("How are you feeling today? ")
+print(f"Hello, {username} is feeling {emotion} today.")
+
+# Ask user for age
+user_input = input("Enter your age: ")
+
+# Convert to integer and subtract 1
+age = int(user_input)
+result = age -1
+
+# Print the result
+print(result)
