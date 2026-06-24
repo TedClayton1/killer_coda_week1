d9914aa9cd5b00654cfccd83026429cd4ebcd40f
Ted <ted_clayton@hotmail.com>
2026-06-24 19:37:06 -0400

    Killr Coda Strings & Input complete for Week 1

--- week_1_kila_coda.py
+++ week_1_kila_coda.py
@@ -1,18 +1,25 @@
-my_age = 49
-current_year = 2026
-birth_year = current_year - my_age
-print(birth_year)
+# my_age = 49
+# current_year = 2026
+# birth_year = current_year - my_age
+# print(birth_year)
 
 username = input("What is your name? ")
 emotion = input("How are you feeling today? ")
 print(f"Hello, {username} is feeling {emotion} today.")
 
-# Ask user for age
-user_input = input("Enter your age: ")
+name = "Ted"
 
-# Convert to integer and subtract 1
-age = int(user_input)
-result = age -1
+print(f"Pleased to meet you {name}.")
+print(f"{name} is feeling happy today!")
 
-# Print the result
-print(result)
+
+
+# # Ask user for age
+# user_input = input("Enter your age: ")
+
+# # Convert to integer and subtract 1
+# age = int(user_input)
+# result = age -1
+
+# # Print the result
+# print(result)
